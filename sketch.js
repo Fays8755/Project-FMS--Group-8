18db9d7355d922983374d0fc695406a110b66722
sumit4183 <sumit4183@yahoo.com>
2021-10-07 12:10:32 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ function setup() {
 function draw() {
     background('pink');
   fill('gray');
-  rect(50, 50, 270, 50);
+  rect(200, 100, 1070, 100);
 
   fill('yellow');
   rect(50, 150, 70, 50);
@@ -14,7 +14,7 @@ function draw() {
   rect(150, 150, 70, 50);
 
   fill('yellow');
-  rect(250, 150, 70, 50);
+  rect(1050, 325, 100, 100, 20);
 
   fill('orange');
   circle(50, 200, 30, 30);
@@ -26,7 +26,7 @@ function draw() {
   circle(250, 200, 30, 30);
   
   fill('red');
-  rect(140, 575, 70, 50);
+  rect(200, 575, 70, 50);
   
   fill('gray');
   circle(1250, 600, 70, 10);
