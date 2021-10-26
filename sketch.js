9670424db8a9d6c2e69d27ee492326238fcbd50f
sumit4183 <sumit4183@yahoo.com>
2021-10-25 21:43:56 -0700

    update

--- sketch.js
+++ sketch.js
@@ -29,6 +29,21 @@ const sentence = ["Fish and visitors stink after three days",
 "Never judge a book by its movie",
 "He who wakes up early, yawns all day long"]
 
+  /*const sentence = ["Fish and visitors stink after three days", 
+  "My wallet is like an onion, opening it makes me cry", 
+  "Friends buy you food. Best friends eat your food", 
+  "My winodws aren't dirty, my dog is painting", 
+  "Every day is a gift, that's why they call it a present", 
+  "I love my job only when I am on vacation",
+  "Don't give up your dreams so soon, sleep longer", 
+  "I'm not lazy, I'm on power saving mode",
+  "Never judge a book by its movie",
+  "He who wakes up early, yawns all day long"]
+    let t = Math.floor(Math.random() * 10);
+    let sen = sentence[t];
+    let h5 = createElement('h5', sen);
+    h5.position(460, 280);*/
+
 
 
 function mainMenu()
