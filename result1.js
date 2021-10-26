fe4f16927f054f31ffe3a16a5492c62176573dc3
Fays <fbekri@asu.edu>
2021-10-26 13:21:50 -0700

    com

--- result1.js
+++ result1.js
@@ -8,3 +8,14 @@ function result1() {
     highScore();
     tryAgainButton();
 }
+var count1;
+var countCorrect1; //amount that matches the sentence length exactly
+var game1Per;
+function score1() {
+    for (i = 0; i < sen.length; i++) {
+        if (sen.charAt(i) == input.charAt(i)) {
+            countCorrect1;
+        }
+    }
+    game1Per = (countCorrect1 / sen.length()) * 100;
+}
