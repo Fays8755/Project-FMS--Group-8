dfadfa55ab3485adf7b3bcf69e42282adef6eda9
Fays <fbekri@asu.edu>
2021-10-28 12:24:37 -0700

    com

--- game2.js
+++ game2.js
@@ -2,6 +2,9 @@ function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
 }
 
+const colors = ['blue', 'red', 'yellow', 'green', 'purple', 'orange', 'black', 'grey', 'white', 'pink'];
+var col;
+var i2 = 1;
 function game2() {
     background('pink');
   
@@ -18,8 +21,19 @@ function game2() {
   
     fill('#BBF291');
     rect(500, 240, 220, 220, 20);
+
+    //red, yellow, blue, green, purple, orange, black, grey, white, pink
   }
   
+  function callColor() {
+    while (i2 == 1) {
+      let t = Math.floor(Math.random() * 10);
+      col = colors[t];
+      fill(col);
+      rect(500, 240, 220, 220, 20);
+      i2 = 2;
+    }
+  }
   function start2() {
     background('pink');
   
